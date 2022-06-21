fd666b7cbc8a1dd68b6b65c11e875476947ebbc3
kaa <ansabanyr13@yandex.ru>
2022-06-21 20:18:59 +0300

    random

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,5); //1 2 3 4
+int numberB = new Random().Next(1,5); //1 2 3 4;
 int result = numberA+numberB;
 Console.WriteLine(result);
