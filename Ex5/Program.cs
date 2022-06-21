6cb86bc0249f878bc63e1c02490731202aff804b
kaa <ansabanyr13@yandex.ru>
2022-06-21 21:14:05 +0300

    first lesson

---
+++ Ex5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "diana")
+{
+    Console.WriteLine("Cool, Diana");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
