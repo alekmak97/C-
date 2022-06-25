c92e8f74a3db9ee310f380e417eb96e9113af01f
kaa <ansabanyr13@yandex.ru>
2022-06-25 12:33:55 +0300

    Ex8

---
+++ Ex8/Program.cs
@@ -0,0 +1,30 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2> result) result = arg2;
+    if (arg3> result) result = arg3;
+    return result;
+}
+
+int a1 = 2;
+int a2 = 7;
+int a3 = 16;
+int b1 = 29;
+int b2 = 70;
+int b3 = 11;
+int c1 = 26;
+int c2 = 78;
+int c3 = 19;
+
+// int max1 = Max(a1, a2, a3);
+// int max2 = Max(b1, b2, b3);
+// int max3 = Max(c1, c2, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, a2, a3), 
+    Max(b1, b2, b3), 
+    Max(c1, c2, c3)
+    );
+
+Console.WriteLine(max);
