fdc81d66cbe8a8a0ae25634a6740ef9d2b92d90c
kaa <ansabanyr13@yandex.ru>
2022-06-25 12:58:26 +0300

    Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {11, 21, 45, 12, 54, 79, 90, 123, 32};
+
+int n = array.Length; // Длина массива
+int find = 90;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // Прерывание, будет показан первый индекс, если числа в массиве совпадают
+    }
+    index++;
+}
