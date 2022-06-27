aab778a190aa3a7003ecf546ae848e96930a03f0
KAA <a@ya.ru>
2022-06-27 17:07:19 +0300

    DZ2 without comments

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,29 +1,29 @@
-﻿// // Lesson 2, Task 10
-// Console.WriteLine("L2-T10");
-// Console.Write("Please write number (type - int), A = ");
-// string a_s = Console.ReadLine();
-// Char[] array1 = a_s.ToCharArray();
-// Console.Write("Вторая цифра числа: ");
-// Console.WriteLine(array1[1]);
-// Console.Write("End L2-T10, please press Enter");
-// Console.ReadLine();
+﻿// Lesson 2, Task 10
+Console.WriteLine("L2-T10");
+Console.Write("Please write number (type - int), A = ");
+string a_s = Console.ReadLine();
+Char[] array1 = a_s.ToCharArray();
+Console.Write("Вторая цифра числа: ");
+Console.WriteLine(array1[1]);
+Console.Write("End L2-T10, please press Enter");
+Console.ReadLine();
 
-// // Lesson 2, Task 13
-// Console.WriteLine("L2-T13");
-// Console.Write("Please write number (type - int), A = ");
-// string aa_s = Console.ReadLine();
-// Char[] array2 = aa_s.ToCharArray();
-// if (array2.Length < 3)
-// {
-//     Console.WriteLine("Третьей цифры нет");
-// }
-// else
-// {
-//     Console.Write("Третья цифра числа: ");
-//     Console.WriteLine(array2[2]);
-// }
-// Console.Write("End L2-T13, please press Enter");
-// Console.ReadLine();
+// Lesson 2, Task 13
+Console.WriteLine("L2-T13");
+Console.Write("Please write number (type - int), A = ");
+string aa_s = Console.ReadLine();
+Char[] array2 = aa_s.ToCharArray();
+if (array2.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.Write("Третья цифра числа: ");
+    Console.WriteLine(array2[2]);
+}
+Console.Write("End L2-T13, please press Enter");
+Console.ReadLine();
 
 // Lesson 2, Task 15
 Console.WriteLine("L2-T15");
