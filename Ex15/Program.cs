6a56119a6e45166b2cc23c6cfd4d9382d26f4f00
KAA <alekmak97@yandex.ru>
2022-07-09 17:00:39 +0300

    Ex15-17

---
+++ Ex15/Program.cs
@@ -0,0 +1,43 @@
+﻿string[,] table = new string[2, 5];
+// 00 01 02 03 04
+// 10 11 12 13 14
+
+// table[1,2] = "слово";
+
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // 0 - ссылка на количество строк
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // 1 - ссылка на количество столбцов
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // 0 - ссылка на количество строк
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // 1 - ссылка на количество столбцов
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matrix = new int[3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ Ex17/Program.cs
@@ -0,0 +1,33 @@
+﻿// Вычисление факториала с помощью рекурсии
+
+double Factorial(int n) // int
+{
+    // 1! = 1
+    // 0! = 1
+    if (n==1) return 1;
+    else return n * Factorial(n-1);
+}
+// Console.WriteLine(Factorial(3));
+
+// for (int i = 1; i < 40; i++) // обратить внимание на тип данных, иначе может быть переполнение
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// Число фибоначчи
+
+// f(1)=1
+// f(2)=1
+// f(n)= f(n-1) + f(n-2)....
+
+double Fibonacci(int n)
+{
+    if (n== 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+
