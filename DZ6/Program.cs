503ae1d225e29e04f96185832016f9ac574c8efd
kaa <ansabanyr13@yandex.ru>
2022-07-11 22:34:10 +0300

    DZ6-7

---
+++ DZ6/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void t41()
+{
+    Console.WriteLine("Введите количество элементов массива, M: ");
+    int M = int.Parse(Console.ReadLine());
+    int[] A = new int[M];
+    int count = 0;
+    for (int i = 0; i < A.Length; i++)
+    {
+        A[i] = new Random().Next(-99, 100);
+        Console.Write("{0} ", A[i]);
+        if (A[i] > 0) count++;
+    }
+    Console.WriteLine();
+    Console.WriteLine("Количество чисел больше 0 = {0}", count);
+}
+t41();
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+void t43()
+{
+    Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2");
+    Console.WriteLine("Введите k1: ");
+    double k1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите k2: ");
+    double k2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите b1: ");
+    double b1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите b2: ");
+    double b2 = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    Console.WriteLine("x = (b2 -b1)/(k1 - k2), y = k2 * x + b2");
+    double x = (b2 - b1) / (k1 - k2); 
+    double y = k2 * x + b2;
+    Console.WriteLine("x = {0:f2}, y = {1:f2}", x, y);
+}
+t43();
---
+++ DZ7/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задайте двумерный массив размером n×m, заполненный случайными вещественными числами
+void t47()
+{
+    Console.Write("Введите количество строк: ");
+    int n = int.Parse(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int m = int.Parse(Console.ReadLine());
+    double[,] table = new double[n,m];
+    Console.WriteLine("table = ");
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i,j] = Convert.ToDouble(new Random().Next(-1000,1001))/100;
+            Console.Write("{0,6}", table[i,j]);
+        }
+        Console.WriteLine();
+    }
+}
+t47();
+
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение 
+// этого элемента или же указание, что такого элемента нет.
+void t50()
+{
+    Console.Write("i: ");
+    int i_n = int.Parse(Console.ReadLine());
+    Console.Write("j_n: ");
+    int j_n = int.Parse(Console.ReadLine());
+    int[,] table = new int[10,10];
+    Console.WriteLine("table = ");
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i,j] = new Random().Next(-99,100);
+            Console.Write("{0,4}", table[i,j]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    if (i_n < table.GetLength(0) && j_n < table.GetLength(1))
+    {
+        Console.WriteLine("{0}", table[i_n,j_n]);
+    }
+    else
+    {
+        Console.WriteLine("Числа нет");
+    }
+}
+t50();
+
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+void t52()
+{
+    Console.Write("Введите количество строк: ");
+    int n = int.Parse(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int m = int.Parse(Console.ReadLine());
+    int[,] table = new int[n,m];
+    double sum = 0;
+    double A = 0;
+    Console.WriteLine("table = ");
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i,j] = new Random().Next(-90,91);
+            Console.Write("{0,4}", table[i,j]);
+        }
+        Console.WriteLine();
+    }
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        sum = 0;
+        for (int i = 0; i < table.GetLength(0); i++)
+        {
+            sum = sum + table[i,j];
+        }
+        A = sum / n;
+        Console.WriteLine("Столбец {0}, A = {1:f2}", j+1, A);
+    }
+
+}
+t52();
