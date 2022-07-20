cd5a423484366565ab4954a24afd2c26ef8da462
KAA <alekmak97@yandex.ru>
2022-07-20 19:15:23 +0300

    Triangle+Classes_Main

---
+++ Ex25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+
+namespace Ex25
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                Console.WriteLine($"Hello, {args[0].Replace("-","")}!");
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
+/*
+Запуск в cmd - \bin\Debug\net6.0\Ex25.exe -Aleksey
+или
+Создать ярлык exe и дописать концовку после exe : -Aleksey
+*/
